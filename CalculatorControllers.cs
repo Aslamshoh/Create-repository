721c912cdb837c732762c86cbe02c83c6def80a2
Асламшох <159581247+Aslamshoh@users.noreply.github.com>
2025-03-07 01:58:15 +0400

    Add files via upload

---
+++ CalculatorControllers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculatorMVC.Models
+{
+    public class CalculatorModel
+    {
+        // Операнды (одно из чисел - long, второе - float)
+        public long Operand1 { get; set; }
+        public float Operand2 { get; set; }
+
+        // Операция (+, -, *, /)
+        public string Operation { get; set; }
+
+        // Результат вычисления (float)
+        public float Result { get; private set; }
+
+        // Метод вычисления
+        public void Calculate()
+        {
+            switch (Operation)
+            {
+                case "+":
+                    Result = Operand1 + Operand2;
+                    break;
+                case "-":
+                    Result = Operand1 - Operand2;
+                    break;
+                case "*":
+                    Result = Operand1 * Operand2;
+                    break;
+                case "/":
+                    if (Operand2 != 0)
+                        Result = Operand1 / Operand2;
+                    else
+                        throw new DivideByZeroException("Ошибка: деление на ноль!");
+                    break;
+                default:
+                    throw new InvalidOperationException("Неизвестная операция");
+            }
+        }
+    }
+}
